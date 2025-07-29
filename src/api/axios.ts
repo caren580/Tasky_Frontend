7ec6b50761f4af8bda6144bba3ca8403f41a296c
caren580 <chumacaren69@gmail.com>
2025-07-29 20:33:44 +0300

    solving issue

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -8,35 +8,35 @@ const axiosInstance = axios.create({
     withCredentials: true
 })
 
-async function getTasks() {
-  try {
-    const response = await axiosInstance.get('/tasks');
-    console.log(response.data);
-  } catch (error) {
-    console.error("Error fetching tasks:", error);
-  }
-}
+// async function getTasks() {
+//   try {
+//     const response = await axiosInstance.get('/tasks');
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error("Error fetching tasks:", error);
+//   }
+// }
 
-getTasks();
+// getTasks();
 
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL;
+// // const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-async function fetchTasks() {
-  try {
-    const response = await fetch(`${VITE_API_URL}/tasks`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error("Error fetching tasks:", error);
-  }
-}
+// async function fetchTasks() {
+//   try {
+//     const response = await fetch(`${VITE_API_URL}/tasks`);
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.error("Error fetching tasks:", error);
+//   }
+// }
 
-// Call the function to fetch tasks
-fetchTasks();
+// // Call the function to fetch tasks
+// fetchTasks();
 
 
 
