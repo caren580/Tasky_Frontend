8e769c5755e35035140292b13a8af06068406c88
caren580 <chumacaren69@gmail.com>
2025-07-29 13:59:41 +0300

    vite env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,12 +1,22 @@
 import  axios  from 'axios'
 
-
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 const axiosInstance = axios.create({
-     baseURL: import.meta.env.VITE_REACT_APP_API_URL 
+     baseURL: VITE_API_URL
     //  || "http://localhost:3000/api"
      ,
     withCredentials: true
 })
 
+async function getTasks() {
+  try {
+    const response = await axiosInstance.get('/tasks');
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+  }
+}
+
+getTasks();
 
 export default axiosInstance
